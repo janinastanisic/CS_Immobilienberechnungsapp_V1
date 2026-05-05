b31f2408f146f0463eb1d642f32cd27a6d2dd75f
Philippe246 <philippe.bloch@student.unisg.ch>
2026-05-06 00:24:35 +0200

    berechnungsfeaauture angepasst und in app.py importiert und eingefügt, Zeilen die jetzt im feature sind, in der app gelöscht

--- feature_berechnung.py
+++ feature_berechnung.py
@@ -1,5 +1,5 @@
 #Berechnung_feature
-
+from feature_machine_learning import ml_basispreis_schaetzen
 
 # ─────────────────────────────────────────────
 # KORREKTURFAKTOREN:Der Faktor wird mit dem Basispreis mulitpliziert und passt den Preis prozentual an. Bsp. Faktor 0.92 = Preis wird um 8% reduziert. Die Faktoren basieren auf Schätzwerten.
@@ -42,7 +42,7 @@ AUSSTATTUNG_LABELS = {
     "hat_seesicht":  "Seesicht",
     "hat_minergie":  "Minergie",
 } #Übersetzung von Bezeichnungen in Texte, welche in der App ersichtlich sind
-
+#überprüfe, ob diese in anderen features verwendet werden, sonst kann mann Austattung_labels löschen
 
 # ─────────────────────────────────────────────
 # BAUJAHR-FAKTOR: 
@@ -60,7 +60,7 @@ def faktor_baujahr(baujahr):
 # BERECHNUNGSFUNKTION: Die Funktion berechne_preis wird definiert
 # ─────────────────────────────────────────────
 def berechne_preis(quartier, zimmerzahl, wohnflaeche, baujahr,
-                   stockwerk, zustand, ausstattung): #Definition einer Funktion mit Eingabewerten
+                   stockwerk, zustand, ausstattung,knn_modell, knn_le, BASISPREIS_PRO_QUARTIER): #Definition einer Funktion mit Eingabewerten
     ml_preis = ml_basispreis_schaetzen(knn_modell, knn_le, quartier, zimmerzahl, jahr=2026) #berechnet den Basispreis: Jahr wird als 2026 gesetzt, da die Schätzung für den heutigen Preis ist
     basispreis = ml_preis if ml_preis is not None else BASISPREIS_PRO_QUARTIER.get(quartier, 11000) #zb. 11k/m^2 und sonst den berechneten durchschnitt unserer Daten als Basispreis
     f_zimmer    = FAKTOR_ZIMMER.get(zimmerzahl, 1.00) #holt den Wert, der bei zimmerzahl als Input angegeben wurde und nimmt den Korrekturfaktor. Falls der Wert nicht gefunden wurde, wird 1.00 als Standardwert verwendet.
