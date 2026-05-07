949113168c414b542f6e6c086968bd3525f46edc
janinastanisic <janina.stanisic@gmail.com>
2026-05-07 16:47:10 +0200

    Faktor_baujahr angepasst

--- app.py
+++ app.py
@@ -1,190 +1,113 @@
-# =============================================================
-# fragebogen.py – Immobilien-Preisschätzer Zürich
-# Ausführen im Terminal: streamlit run fragebogen.py
-# =============================================================
-
-import streamlit as st #importiert das Framework Streamlit, mit der Abkürzung st
-import plotly.graph_objects as go #importiert eine Bibliothek für interaktive Diagramme mit der low-level Variante go
-import plotly.express as px #???? raus nehmen, nutzen wir nicht???? Bibliothek für interaktive Diagramme, px als high-level Variante
-import folium #importiert interaktive Landkarten
-from streamlit_folium import st_folium #Bindeglied, damit die Karte in Streamlit angezeigt werden kann
-from feature_dataset import get_daten #importiert get_daten vom Dataset Feature
-from feature_Koordinaten import get_koordinaten
-from feature_heatmap_chart import erstelle_heatmap_karte
-from feature_machine_learning import trainiere_knn_modell, ml_basispreis_schaetzen
-from feature_waterfall_chart import erstelle_waterfall_chart 
-from feature_gauge_chart import erstelle_gauge_chart
-from feature_berechnung import berechne_preis, FAKTOR_ZUSTAND, FAKTOR_STOCKWERK, AUSSTATTUNG_FAKTOREN, AUSSTATTUNG_LABELS
-# Seitenkonfiguration von Streamlit: Titel setzen und mittig zentrieren
-st.set_page_config(
-    page_title="Immobilien-Preisschätzer Zürich",
-    layout="centered"
-)
+#Berechnung_feature
+from feature_machine_learning import ml_basispreis_schaetzen
 
 # ─────────────────────────────────────────────
-# BASISPREISE PRO QUARTIER (CHF pro m²)
-# ─────────────────────────────────────────────
-df= get_daten()
-#Daten werden einmalig geladen (aus Feature Dataset.py)
-BASISPREIS_PRO_QUARTIER = (
-    df.groupby("Quartier")["Preis_pro_m2"]
-    .mean()
-    .round()
-    .astype(int)
-    .to_dict()
-)
-
-#ML-Modell wird beim start einmalig trainiert
-knn_modell, knn_le, _, _, _ = trainiere_knn_modell(df)
-
-# ─────────────────────────────────────────────
-# KOORDINATEN DER QUARTIERE (Mittelpunkte)
+# KORREKTURFAKTOREN:Der Faktor wird mit dem Basispreis mulitpliziert und passt den Preis prozentual an. Bsp. Faktor 0.92 = Preis wird um 8% reduziert.
 # ─────────────────────────────────────────────
 
-QUARTIER_KOORDINATEN = get_koordinaten()
+FAKTOR_ZUSTAND = { 
+    "Neuwertig / Neubau":    1.125, 
+    "Gut gepflegt":          1.075,
+    "Renovationsbeduerftig": 1.00,
+}
+#Gemaess Frey (2026) fuehrt ein neuwertiger Zustand zu einer Wertsteigerung von 10 bis 15 %, waehrend fuer einen guten Zustand 
+#eine Wertsteigerung von 5 bis 10 % angegeben wird. Fuer die vorliegende Bewertung wurde jeweils der Mittelwert der in der 
+#Quelle genannten Spannbreiten als Korrekturfaktor verwendet, sodass 12,5 % fuer den neuwertigen sowie 7,5 % fuer den guten Zustand gewaelt wurden.
+
+FAKTOR_STOCKWERK = {
+    "Erdgeschoss":                   1.00,
+    "1. Obergeschoss":               1.022,
+    "2. Obergeschoss":               1.044,
+    "3. Obergeschoss":               1.066,
+    "4. Obergeschoss":               1.088,
+    "5. Obergeschoss":               1.11,
+    "6. Obergeschoss":               1.132,
+    "7. Obergeschoss":               1.154,
+    "8. Obergeschoss":               1.176,
+    "9. Obergeschoss":               1.198,
+    "10. Obergeschoss oder hoeher":  1.22,
+}
+#Gemaess Conroy et al. (1013, S.201) geht ein hoeheres Stockwerk mit einem Anstieg des Immobilienpreises von 2.2 Prozent einher. 
+
+AUSSTATTUNG_FAKTOREN = {
+    "hat_balkon":    0.1385,
+    "hat_tiefgarage": 0.10,
+    "hat_lift":      0.00,#wird nicht direkt verwendet, Faktor_Lift wird stockwerkabhaengig in berechne_preis() berechnet
+    "hat_seesicht":  0.11,
+    "hat_minergie":  0.491,
+} #Jede zusätzliche Ausstattung addiert einen Prozentsatz zum Preis: Bsp. Faktor 0.03 = +3%.
+#Gemaess Chau et al. (2004, S. 256) fuehrt ein grosser Balkon mit guter Aussicht zu 24 Prozent hoeherem Kaufpreis und ein kleiner Balkon ohne Aussicht 
+#zu 3.7% hoeherem Kaufpreis. Fuer den gewaehlten Korrekturfaktor hat_balkon von 13.85 Prozent haben wir daraus den Mittelwert berechnet. 
+#Gemaess Deschermeier et al. (2023, S. 46) steigert eine Tiefgarage den Immobilienwert um durchschnittlich 10 Prozent.
+#Gemaess Niklowitz (2026) erhoeht eine Seesicht den Immobilienpreis um 11 Prozent.
+#Gemaess Kempf & Syz (2022, S. 170) hat die Stadt Zuerich eine Minergie Preispraemie von 4.91 Prozent.
+
+AUSSTATTUNG_LABELS = {
+    "hat_balkon":    "Balkon / Terrasse",
+    "hat_tiefgarage": "Tiefgarage",
+    "hat_lift":      "Lift",
+    "hat_seesicht":  "Seesicht",
+    "hat_minergie":  "Minergie",
+} #Übersetzung von Bezeichnungen in Texte, welche in der App ersichtlich sind
+#überprüfe, ob diese in anderen features verwendet werden, sonst kann mann Austattung_labels löschen
 
 # ─────────────────────────────────────────────
+# BAUJAHR-FAKTOR: 
+# ─────────────────────────────────────────────
+def faktor_baujahr(baujahr):
+    alter = 2026 - baujahr #Alter der Immobilie wird berechnet
+    faktor = 1 - ((80 - alter) / 80) #Berechnet den Multiplikator direkt: Bsp. Alter 0 = Faktor 0.0, Alter 40 = Faktor 0.50, Alter 80 = Faktor 1.0
+    faktor = max(0.01, min(faktor, 1.0)) #Begrenzt den Faktor auf 0.01 bis 1.0, damit keine negativen oder über 1.0 liegenden Werte entstehen
+    return faktor
+#Die gewaehlte Formel basiert auf der Berechnung der Alterswertminderung gemaess Gutknecht (n.d.). Dabei wird eine Gesamtnutzungsdauer von 80 Jahren angenommen. 
 
-# =============================================================
-# STREAMLIT APP
-# =============================================================
-
-st.title("Immobilien-Preisschätzer Zürich") #Erstellt den Titel in Streamlit
-st.write("Gib die Eigenschaften deiner Immobilie ein - wir berechnen den geschätzten Marktwert.") #Erstellt den Beschreibungstext in Streamlit
-st.markdown("---") #Erstellt eine horizontale Trennlinie in Streamlit
-
-# ── 1. LAGE ──
-st.subheader("Lage") #Erstellt einen Untertitel in Streamlit
-QUARTIERE = ["— Bitte waehlen —"] + sorted(BASISPREIS_PRO_QUARTIER.keys()) #Zwei Listen werden erstellt und miteinander verbunden. Die erste besteht aus dem Platzhalter: Bitte waehlen. Die zweite Liste besteht aus allen Schlüsseln (Quartiernamen) des Dictionaries, welche alphabetisch sortiert werden.
-quartier  = st.selectbox("In welchem Stadtquartier liegt die Immobilie?", options=QUARTIERE) #Ein Dropdown Menü wird in Streamlit erstellt mit einem Beschriftungstext. Das Dropdown Menu beinhaltet eine Liste aller Optionen, die in der vorherigen Zeile definiert wurde. 
-
-# ── 2. GROESSE ──
-st.subheader("Grösse") #Erstellt einen Untertitel in Streamlit
-col1, col2 = st.columns(2) #Die Seite wird in zwei gleich breite Spalten aufgeteilt. col1 links und col2 rechts.
-with col1: #Definiert, was in der linken Spalte angezeigt wird
-    zimmerzahl = st.selectbox( #Erstellt ein Dropdown Menü und speichert den Eingabewert unter zimmerzahl ab
-        "Anzahl Zimmer", #Definiert den Text über dem Dropdown Menu
-        options=["1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5+"], #Beschreibt die Liste aller auswählbaren Optionen
-        index=0 #Definiert den Standardwert bei Index 0 --> 1 Zimmer
-    )
-with col2: #Definiert, was in der rechten Spalte angezeigt wird
-    wohnflaeche = st.number_input( #Erstellt ein Eingabefeld und speichert den Eingabewert unter wohnflaeche
-        "Wohnfläche (m2)", min_value=10, max_value=500, value=10, step=5 #Definiert die kleinste erlaubte Zahl, die grösste erlaubte Zahl, den Standardwert und die Steps (wenn man auf + klickt, springt die Zahl um diesen Wert)
-    )
-
-# ── 3. GEBAEUDE ──
-st.subheader("Gebäude") #Erstellt einen Untertitel in Streamlit
-col3, col4 = st.columns(2) #Die Seite wird in zwei gleich breite Spalten aufgeteilt. col3 links und col4 rechts.
-with col3: #Definiert die linke Seite
-    baujahr = st.slider("Baujahr", min_value=1900, max_value=2026, value=2026) #Erstellt einen Schieberegler in Streamlit mit dem Beschriftungstext, dem Mindestwert, dem Maximalwert und dem Standardwert und speichert den Eingabewert unter baujahr
-with col4: #Definiert die rechte Seite
-    stockwerk = st.selectbox( #Erstellt ein Dropdown Menu und speichert den Eingabewert unter stockwerk ab
-        "Stockwerk", #Definiert den Text über dem Dropdown Menü
-        options=[ #Beschreibt die Liste aller auswählbaren Optionen, Erdgeschoss wird mit index 0 als Stadardwert gezeigt
-            "Erdgeschoss", "1. Obergeschoss", "2. Obergeschoss",
-            "3. Obergeschoss", "4. Obergeschoss",
-            "5. Obergeschoss", "6. Obergeschoss",
-            "7. Obergeschoss", "8. Obergeschoss",
-            "9. Obergeschoss", "10. Obergeschoss oder hoeher"
-        ]
-    )
-
-# ── 4. ZUSTAND ──
-st.subheader("Zustand") #Erstellt einen Untertitel in Streamlit
-zustand = st.radio( #Erstellt buttons, von denen der User eine Option wählen kann
-    "Wie ist der aktuelle Renovationsstand?", #Definiert den Text über den Buttons
-    options=["Neuwertig / Neubau", "Gut gepflegt", "Renovationsbeduerftig"], #Definiert die Liste aller auswählbaren Optionen
-    index=1, #Setzt Gut gepflegt als Standardwert
-    horizontal=True #Formatiert die Buttons horizontal, also nebeneinander
-)
-
-# ── 5. AUSSTATTUNG ──
-st.subheader("Ausstattung") #Erstellt einen Untertitel in Streamlit
-col5, col6 = st.columns(2) #Die Seite wird in zwei gleich breite Spalten aufgeteilt. col5 links und col6 rechts
-with col5: #Definiert die linke Seite
-    hat_balkon    = st.checkbox("Balkon / Terrasse") #Erstellt Checkboxen, welche den Wert als True speichert, wenn die Checkbox aktiviert wurde und als False, wenn sie nicht aktiviert wurde. Die Standardwerte sind False
-    hat_tiefgarage = st.checkbox("Tiefgarage")
-    hat_lift      = st.checkbox("Lift im Gebäude")
-with col6:
-    hat_seesicht = st.checkbox("Seesicht / Aussicht")
-    hat_minergie = st.checkbox("Minergie-Standard") #Erstellt Checkboxen, welche den Wert als True speichert, wenn die Checkbox aktiviert wurde und als False, wenn sie nicht aktiviert wurde. Die Standardwerte sind False
-
-# ── 6. BERECHNUNG & ERGEBNIS ──
-st.markdown("---") #Erstellt eine horizontale Trennlinie in Streamlit
-berechnen = st.button("Marktwert berechnen") #Erstellt einen Button mit dem Text Marktwert berechnen, wenn der Button angeglickt wird, wird der Wert True in der variable berechnen gespeichert. Ansonsten False
-
-# Session State initialisieren – speichert Ergebnisse über Neuladen hinweg. Erstellt leeren Platz für Ergebnis beim Start der App. Wird päter mit dem berechneten Preis überschrieben, sobald User auf Marktwert berechnen klickt.
-if "ergebnis" not in st.session_state:
-    st.session_state.ergebnis = None
-
-if berechnen: #Sofern der Button Marktwert berechnen geklickt wurde, ist diese if-Bedingung true
-    if quartier == "— Bitte wählen —":
-        st.error("Bitte wähle ein Stadtquartier aus.") #Erstellt eine Fehlermeldung mit dem Text Bitte wähle ein Stadtquartier aus, wenn bei quartier nichts angewählt wurde
+def lift_faktor_berechnen(stockwerk):
+    #Berechnet den Lift-Faktor abhängig vom Stockwerk.
+    #Erdgeschoss: kein Effekt (Faktor 0)
+    #1.-2. OG: +1.59% 
+    #3.-5. OG: +4.58% 
+    #6.-10. OG: +8.10% Gemaess Dai et al. (2026, S. 21) erhoeht ein Lift im Gebaede den Wohnungspreis bei unteren Stockwerken um 1.59 Prozent, bei mittleren um 4.58% und bei hoehern um 8.10 Prozent.
+    if stockwerk == "Erdgeschoss": #Wenn das Stockwerk Erdgeschoss angegeben wurde, wird kein Faktor dazugerechnet.
+        return 0.00
+    elif stockwerk in ["1. Obergeschoss", "2. Obergeschoss"]:#Prüft, ob der eingegebene Wert in der Liste 1. Obergeschoss, 2. Obergeschoss vorkommt. Wenn das True ist, nimmt es den Faktor 0.0159
+        return 0.0159
+    elif stockwerk in ["3. Obergeschoss", "4. Obergeschoss", "5. Obergeschoss"]: #Prüft, ob der eingegebene Wert in der List 3. Obergeschoss, 4. Obergeschoss, 5. Obergeschoss vorkommt. Wenn das True ist, nimmt es den Faktor 0.0458
+        return 0.0458
+    elif stockwerk in ["6. Obergeschoss", "7. Obergeschoss", "8. Obergeschoss", #Prüft, ob der eingegebene Wert in der List 6. Obergeschoss, 7. Obergeschoss, 8. Obergeschoss, 9. Obergeschoss, 10. Obergeschoss oder hoeher vorkommt. Wenn das True ist, nimmt es den Faktor 0.0810
+                       "9. Obergeschoss", "10. Obergeschoss oder hoeher"]:
+        return 0.0810
     else:
-        ausstattung = {
-            "hat_balkon":    hat_balkon,
-            "hat_tiefgarage": hat_tiefgarage,
-            "hat_lift":      hat_lift,
-            "hat_seesicht":  hat_seesicht,
-            "hat_minergie":  hat_minergie, #Wenn die obige if Bedingung nicht erfüllt ist, werden alle Ausstattungswerte, welche angegeben wurden, in einem Dictionary zusammengefasst
-        }
+        return 0.00 #Fallback falls unbekanntes Stockwerk
 
-        preis_pro_m2, gesamtpreis, faktoren = berechne_preis( #Ruft die in Zeile 122-148 definierte Funktion ab und übergibt die Angaben des Users
-            quartier, zimmerzahl, wohnflaeche,
-            baujahr, stockwerk, zustand, ausstattung, 
-            knn_modell,knn_le, BASISPREIS_PRO_QUARTIER
-        )
+# ─────────────────────────────────────────────
+# BERECHNUNGSFUNKTION: Die Funktion berechne_preis wird definiert
+# ─────────────────────────────────────────────
+def berechne_preis(quartier, zimmerzahl, wohnflaeche, baujahr,
+                   stockwerk, zustand, ausstattung,knn_modell, knn_le, BASISPREIS_PRO_QUARTIER): #Definition einer Funktion mit Eingabewerten
+    ml_preis = ml_basispreis_schaetzen(knn_modell, knn_le, quartier, zimmerzahl, jahr=2026) #berechnet den Basispreis: Jahr wird als 2026 gesetzt, da die Schätzung für den heutigen Preis ist
+    basispreis = ml_preis if ml_preis is not None else BASISPREIS_PRO_QUARTIER.get(quartier, 11000) #zb. 11k/m^2 und sonst den berechneten durchschnitt unserer Daten als Basispreis
+    f_zustand   = FAKTOR_ZUSTAND.get(zustand, 1.00) #holt den Wert, der bei zustand als Input angegeben wurde und nimmt den Korrekturfaktor. Falls der Wert nicht gefunden wurde, wird 1.00 als Standardwert verwendet.
+    f_stockwerk = FAKTOR_STOCKWERK.get(stockwerk, 1.00) #holt den Wert, der bei stockwerk als Input angegeben wurde und nimmt den Korrekturfaktor. Falls der Wert nicht gefunden wurde, wird 1.00 als Standardwert verwendet.
+    f_baujahr   = faktor_baujahr(baujahr) #holt den Wert, der bei baujahr als Input angegeben wurde und nimmt den Korrekturfaktor.
 
-        # Ergebnis im Session State speichern
-        st.session_state.ergebnis = { #überschreibt das none im session state mit den berechneten und eingegebenen Werten
-            "preis_pro_m2": preis_pro_m2,
-            "gesamtpreis":  gesamtpreis,
-            "faktoren":     faktoren,
-            "quartier":     quartier,
-            "ml_basispreis": faktoren["Basispreis (Quartier)"],
-        }
+    f_ausstattung = 1.00 #Startet bei 1.00. 
+    for merkmal, wert in ausstattung.items(): #Iteriert mit einer for Schleife durch alle Ausstattungsmerkmale durch
+        if wert: #Nur wenn eine Checkbox aktiviert ist (deren Wert = True) wird der nächste Schritt durchgeführt
+            if merkmal == "hat_lift": #Lift-Faktor ist abhängig vom Stockwerk
+                f_ausstattung += lift_faktor_berechnen(stockwerk) #Ruft die Hilfsfunktion auf und addiert den stockwerkabhängigen Lift-Faktor
+            else:
+                f_ausstattung += AUSSTATTUNG_FAKTOREN.get(merkmal, 0) #Holt den Faktor für das Ausstattungsmerkmal aus dem obigen Dictionary und addiert ihn zu 1.00
+    
+    preis_pro_m2 = (basispreis * f_zustand
+                    * f_stockwerk * f_baujahr * f_ausstattung) #Berechnet den Preis pro Quadratmeter, indem es unser durch das ML-modell kalulierter Basispreis mit allen Korrekturfaktoren multipliziert
+    gesamtpreis  = preis_pro_m2 * wohnflaeche #Berechnet den Gesamtpreis indem der Preis pro Quadratmeter mit der wohnflaeche als Input Multipliziert wird
 
-# Ergebnis anzeigen – bleibt sichtbar solange session_state gefüllt ist: Sofern Ergebnisse im Session state abgespeichert wurden, wird eine Kurzform für session state definiert
-if st.session_state.ergebnis:
-    e = st.session_state.ergebnis
+    faktoren = { #speichert die berechneten Faktoren als Dictionary ab
+        "Basispreis (Quartier)": basispreis,
+        "Zustand":               f_zustand,
+        "Stockwerk":             f_stockwerk,
+        "Baujahr":               f_baujahr,
+        "Ausstattung":           f_ausstattung,
+    }
 
-    st.markdown("### Geschätzter Marktwert") #Erstellt einen mittelgrossen Titel in Streamlit
-    col_r1, col_r2 = st.columns(2) #Die Seite wird in zwei gleich breite Spalten aufgeteilt. col_r1 links und col_r2 rechts
-    with col_r1: #Definiert die linke Seite
-        st.metric( #Formatiert die nächsten Zeilen als Kennzahlen (Kleiner Text + Grosse Zahl)
-            label="Geschätzter Kaufpreis", #Kleiner Text
-            value=f"CHF {e['gesamtpreis']:,.0f}".replace(",", "'") #Grosse Zahl, die berechnet wurde. Zahl wird mit Hochkommas als Tausendertrennzeichen dargestellt
-        )
-    with col_r2: #Definiert die rechte Seite
-        st.metric(#Formatiert die nächsten Zeilen als Kennzahlen (Kleiner Text + Grosse Zahl)
-            label="Preis pro m2", #Kleiner Text
-            value=f"CHF {e['preis_pro_m2']:,.0f}".replace(",", "'") #Grosse Zahl, die berechnet wurde. Zahl wird mit Hochkommas als Tausendertrennzeichen dargestellt
-        )
-
-    st.markdown("---") #Erstellt eine horizontale Trennlinie in Streamlit
-
- 
-
-    # Chart 1: Waterfall
-    st.markdown("### Preiszusammensetzung – Schritt für Schritt")
-    st.caption("Wie sich der Endpreis aus dem Basispreis und den einzelnen Faktoren aufbaut.")
-    fig_waterfall = erstelle_waterfall_chart(e["faktoren"], e["preis_pro_m2"])
-    st.plotly_chart(fig_waterfall, width="stretch")
-
-    # Chart 2: Gauge
-    st.markdown("### Preis im Marktvergleich")
-    st.caption(
-        f"Der grüne Strich zeigt den Basispreis für {e['quartier']}. "
-        "Die farbigen Zonen zeigen günstig (grün), mittel (gelb) und teuer (rot) im Vergleich zu allen Zürcher Quartieren."
-    )
-    fig_gauge = erstelle_gauge_chart(e["preis_pro_m2"], e["quartier"], e["ml_basispreis"], BASISPREIS_PRO_QUARTIER)
-    st.plotly_chart(fig_gauge, width="stretch")
-
-    # Chart 3: Heatmap
-    st.markdown("### Preisübersicht Zürich – Heatmap")
-    st.caption(
-        "Grün = günstigere Quartiere, Rot = teurere Quartiere. "
-        "Ihr Quartier ist dunkel umrandet."
-    )
-    karte = erstelle_heatmap_karte(ausgewaehltes_quartier=e["quartier"], quartier_koordinaten=QUARTIER_KOORDINATEN, basispreis_pro_quartier=BASISPREIS_PRO_QUARTIER)
-    st_folium(karte, use_container_width=True, height=450)
+    return round(preis_pro_m2), round(gesamtpreis), faktoren #gibt den gerundeten Preis pro m2, den gerundeten Gesamtpreis und das Dictionary der Faktoren zurück
